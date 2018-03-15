49c4ac2198ae580e5d7f3b6df1eeace6f0aa8758
Renal Apriansyah <renal>
2018-03-15 12:56:54 +0700

    nambah cara yang lebih fleksibel

--- total digit rekursif.js
+++ total digit rekursif.js
@@ -20,6 +20,25 @@ else if(angka<100000){
  
 }
 //512/100 = 5 + 512%100 = 12 + 12%10 + 2
+
+function totalDigitRekursif(angka) {
+if(angka<=0){
+  return 0
+}
+else if(angka<10){
+  return Math.floor(angka/1)
+}
+else if(angka>10){
+ return (angka%10) + Math.floor(totalDigitRekursif(angka/10))
+}
+}
+
+// TEST CASES
+console.log(totalDigitRekursif(512)); // 8
+console.log(totalDigitRekursif(1542)); // 12
+console.log(totalDigitRekursif(5)); // 5
+console.log(totalDigitRekursif(21)); // 3
+console.log(totalDigitRekursif(11111)); // 5
 // TEST CASES
 console.log(totalDigitRekursif(512)); // 8
 console.log(totalDigitRekursif(1542)); // 12
